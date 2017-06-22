89ec34c5be0ba8903105d9ab4fcb09e028dddda3
Zongjian ZHANG <Zongjian.Zhang@data61.csiro.au>
2017-06-22 18:52:05 +1000

    some annotations

--- C5_FasterRCNN_KERAS/keras_frcnn/data_generators.py
+++ C5_FasterRCNN_KERAS/keras_frcnn/data_generators.py
@@ -1,13 +1,9 @@
 import numpy as np
 import cv2
 import random
-import copy
 from keras_frcnn import data_augment
-from keras_frcnn import roi_helpers
 import threading
 import itertools
-
-# random.seed(0)
 
 
 def get_img_output_length(width, height):
@@ -74,30 +70,34 @@ class SampleSelector:
     def __init__(self, class_count):
         # ignore classes that have zero samples
         self.classes = [b for b in class_count.keys() if class_count[b] > 0]
+        # set up an iterator for self.classes
         self.class_cycle = itertools.cycle(self.classes)
         self.curr_class = next(self.class_cycle)
 
     def skip_sample_for_balanced_class(self, img_data):
 
         class_in_img = False
-
+        # check whether img_data includes a bbox for current class
+        # if yes, skip current class
         for bbox in img_data['bboxes']:
-
             cls_name = bbox['class']
-
             if cls_name == self.curr_class:
                 class_in_img = True
                 self.curr_class = next(self.class_cycle)
                 break
-
+        # return whether to use this img_data
+        # not use this skipped class and img_data  
         if class_in_img:
             return False
         else:
             return True
 
-
+# two kinds of positive anchors:
+# 1) with IoU larger than rpn_max_overlap (0.7)
+# 2) with the best IoU (<=0.7) that a GT bbox has 
 def calc_rpn(C, img_data, width, height, resized_width, resized_height):
 
+    # parameters for rpn
     downscale = float(C.rpn_stride)
     anchor_sizes = C.anchor_box_scales
     anchor_ratios = C.anchor_box_ratios
@@ -108,19 +108,25 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height):
 
     n_anchratios = len(anchor_ratios)
     
-    # initialise empty output objectives
+    # initialise empty output objectives for 9 anchors at each position:
+    # positive or negative overlap 
     y_rpn_overlap = np.zeros((output_height, output_width, num_anchors))
+    # valid as training data
     y_is_box_valid = np.zeros((output_height, output_width, num_anchors))
+    # regression offsets (x,y,w,h) with best IoU
     y_rpn_regr = np.zeros((output_height, output_width, num_anchors * 4))
 
+    # number of bbox from ground truth (GT)
     num_bboxes = len(img_data['bboxes'])
 
-    num_anchors_for_bbox = np.zeros(num_bboxes).astype(int)
-    best_anchor_for_bbox = -1*np.ones((num_bboxes, 4)).astype(int)
-    best_iou_for_bbox = np.zeros(num_bboxes).astype(np.float32)
-    best_x_for_bbox = np.zeros((num_bboxes, 4)).astype(int)
-    best_dx_for_bbox = np.zeros((num_bboxes, 4)).astype(np.float32)
+    # set up variables for each groundth bbox
+    num_anchors_for_bbox = np.zeros(num_bboxes).astype(int) # anchor numbers for each bbox
+    best_anchor_for_bbox = -1*np.ones((num_bboxes, 4)).astype(int) # best anchor for each bbox
+    best_iou_for_bbox = np.zeros(num_bboxes).astype(np.float32) # best IoU for each bbox
+    best_x_for_bbox = np.zeros((num_bboxes, 4)).astype(int) # coordinates of best anchor
+    best_dx_for_bbox = np.zeros((num_bboxes, 4)).astype(np.float32) # offsets of best anchor
 
+    # resize GT bbox coordinates acoording to resized image
     # get the GT box coordinates, and resize to account for image resizing
     gta = np.zeros((num_bboxes, 4))
     for bbox_num, bbox in enumerate(img_data['bboxes']):
@@ -131,28 +137,35 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height):
         gta[bbox_num, 3] = bbox['y2'] * (resized_height / float(height))
     
     # rpn ground truth
-
+    # loop each anchor size and ratio
     for anchor_size_idx in range(len(anchor_sizes)):
         for anchor_ratio_idx in range(n_anchratios):
+            
+            # get actual anchor size
             anchor_x = anchor_sizes[anchor_size_idx] * anchor_ratios[anchor_ratio_idx][0]
             anchor_y = anchor_sizes[anchor_size_idx] * anchor_ratios[anchor_ratio_idx][1]    
             
-            for ix in range(output_width):                    
-                # x-coordinates of the current anchor box    
+            # loop each column in the output map
+            for ix in range(output_width):  
+                                  
+                # x-coordinates of the current anchor box on the input size 
+                # 0.5 helps map to the center of anchor box
                 x1_anc = downscale * (ix + 0.5) - anchor_x / 2
                 x2_anc = downscale * (ix + 0.5) + anchor_x / 2    
                 
-                # ignore boxes that go across image boundaries                    
+                # ignore boxes that go beyond image boundaries                    
                 if x1_anc < 0 or x2_anc > resized_width:
                     continue
-                    
+                
+                # loop each row in the output map
                 for jy in range(output_height):
 
-                    # y-coordinates of the current anchor box
+                    # y-coordinates of the current anchor box  on the input size
+                    # 0.5 helps map to the center of anchor box
                     y1_anc = downscale * (jy + 0.5) - anchor_y / 2
                     y2_anc = downscale * (jy + 0.5) + anchor_y / 2
 
-                    # ignore boxes that go across image boundaries
+                    # ignore boxes that go beyond image boundaries
                     if y1_anc < 0 or y2_anc > resized_height:
                         continue
 
@@ -163,29 +176,44 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height):
                     # note that this is different from the best IOU for a GT bbox
                     best_iou_for_loc = 0.0
 
+                    # check each GT bbox for current anchor (size, ratio, position)
                     for bbox_num in range(num_bboxes):
                         
                         # get IOU of the current GT box and the current anchor box
                         curr_iou = iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]], [x1_anc, y1_anc, x2_anc, y2_anc])
-                        # calculate the regression targets if they will be needed
+                        # calculate the regression targets if they will be needed based on 2 conditions:
+                        # 1) if current iou is larger than the best one that current GT bbox has 
+                        # 2) or if current iou larger than the parameter: rpn_max_overlap
                         if curr_iou > best_iou_for_bbox[bbox_num] or curr_iou > C.rpn_max_overlap:
+                            
+                            # center for current GT bbox
                             cx = (gta[bbox_num, 0] + gta[bbox_num, 1]) / 2.0
                             cy = (gta[bbox_num, 2] + gta[bbox_num, 3]) / 2.0
+                            
+                            # center for current anchor bbox
                             cxa = (x1_anc + x2_anc)/2.0
                             cya = (y1_anc + y2_anc)/2.0
 
+                            # center offset                  
                             tx = (cx - cxa) / (x2_anc - x1_anc)
                             ty = (cy - cya) / (y2_anc - y1_anc)
+                            
+                            # size offset
                             tw = np.log((gta[bbox_num, 1] - gta[bbox_num, 0]) / (x2_anc - x1_anc))
                             th = np.log((gta[bbox_num, 3] - gta[bbox_num, 2]) / (y2_anc - y1_anc))
                         
+                        # not backgound classes
                         if img_data['bboxes'][bbox_num]['class'] != 'bg':
 
                             # all GT boxes should be mapped to an anchor box, so we keep track of which anchor box was best
                             if curr_iou > best_iou_for_bbox[bbox_num]:
+                                # record position in output map, size, and ratio
                                 best_anchor_for_bbox[bbox_num] = [jy, ix, anchor_ratio_idx, anchor_size_idx]
+                                # record best iou score
                                 best_iou_for_bbox[bbox_num] = curr_iou
+                                # record coordinates
                                 best_x_for_bbox[bbox_num,:] = [x1_anc, x2_anc, y1_anc, y2_anc]
+                                # record offset
                                 best_dx_for_bbox[bbox_num,:] = [tx, ty, tw, th]
 
                             # we set the anchor to positive if the IOU is >0.7 (it does not matter if there was another better box, it just indicates overlap)
@@ -205,43 +233,41 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height):
 
                     # turn on or off outputs depending on IOUs
                     if bbox_type == 'neg':
+                        # set as valid training data and negative
                         y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
                         y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
                     elif bbox_type == 'neutral':
                         y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
                         y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
                     elif bbox_type == 'pos':
+                        # set as valid training data and positive
                         y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
                         y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+                        # save best offsets for regression
                         start = 4 * (anchor_ratio_idx + n_anchratios * anchor_size_idx)
                         y_rpn_regr[jy, ix, start:start+4] = best_regr
 
-    # we ensure that every bbox has at least one positive RPN region
-
+    # ensure that every GT bbox has at least one positive RPN region
     for idx in range(num_anchors_for_bbox.shape[0]):
         if num_anchors_for_bbox[idx] == 0:
             # no box with an IOU greater than zero ...
             if best_anchor_for_bbox[idx, 0] == -1:
                 continue
-            y_is_box_valid[
-                best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios *
-                best_anchor_for_bbox[idx,3]] = 1
-            y_rpn_overlap[
-                best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios *
-                best_anchor_for_bbox[idx,3]] = 1
+            # set best anchor as positive
+            y_is_box_valid[best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios * best_anchor_for_bbox[idx,3]] = 1
+            y_rpn_overlap[best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], best_anchor_for_bbox[idx,2] + n_anchratios * best_anchor_for_bbox[idx,3]] = 1
             start = 4 * (best_anchor_for_bbox[idx,2] + n_anchratios * best_anchor_for_bbox[idx,3])
-            y_rpn_regr[
-                best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], start:start+4] = best_dx_for_bbox[idx, :]
-
+            y_rpn_regr[best_anchor_for_bbox[idx,0], best_anchor_for_bbox[idx,1], start:start+4] = best_dx_for_bbox[idx, :]
+    
+    # set size and ratio as the first channel, and add first dimension
     y_rpn_overlap = np.transpose(y_rpn_overlap, (2, 0, 1))
-    y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0)
-
+    y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0)    
     y_is_box_valid = np.transpose(y_is_box_valid, (2, 0, 1))
     y_is_box_valid = np.expand_dims(y_is_box_valid, axis=0)
-
     y_rpn_regr = np.transpose(y_rpn_regr, (2, 0, 1))
     y_rpn_regr = np.expand_dims(y_rpn_regr, axis=0)
-
+    
+    # get positions of positive and negative rpn anchors
     pos_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 1, y_is_box_valid[0, :, :, :] == 1))
     neg_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 0, y_is_box_valid[0, :, :, :] == 1))
 
@@ -250,16 +276,17 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height):
     # one issue is that the RPN has many more negative than positive regions, so we turn off some of the negative
     # regions. We also limit it to 256 regions.
     num_regions = 256
-
+    # select positive rpn anchors randomly
     if len(pos_locs[0]) > num_regions/2:
         val_locs = random.sample(range(len(pos_locs[0])), len(pos_locs[0]) - num_regions/2)
         y_is_box_valid[0, pos_locs[0][val_locs], pos_locs[1][val_locs], pos_locs[2][val_locs]] = 0
         num_pos = num_regions/2
-
+    # select negative rpn anchors randomly
     if len(neg_locs[0]) + num_pos > num_regions:
         val_locs = random.sample(range(len(neg_locs[0])), len(neg_locs[0]) - num_pos)
         y_is_box_valid[0, neg_locs[0][val_locs], neg_locs[1][val_locs], neg_locs[2][val_locs]] = 0
 
+    # reformat by concatenateing
     y_rpn_cls = np.concatenate([y_is_box_valid, y_rpn_overlap], axis=1)
     y_rpn_regr = np.concatenate([np.repeat(y_rpn_overlap, 4, axis=1), y_rpn_regr], axis=1)
 
@@ -292,30 +319,33 @@ def threadsafe_generator(f):
 
 def get_anchor_gt(all_img_data, class_count, C, backend, mode='train'):
     
+    # sort all_img_data
     all_img_data = sorted(all_img_data, key=lambda x:sorted(x.keys()))
     
+    # set sample selector 
     sample_selector = SampleSelector(class_count)
 
     while True:
+        # shuffle for training mode
         if mode == 'train':
             random.shuffle(all_img_data)
 
+        # process each image data
         for img_data in all_img_data:
             try:
-
+                # skip current img_data for class balance
                 if C.balanced_classes and sample_selector.skip_sample_for_balanced_class(img_data):
                     continue
 
                 # read in image, and optionally add augmentation
-
                 if mode == 'train':
                     img_data_aug, x_img = data_augment.augment(img_data, C, augment=True)
                 else:
                     img_data_aug, x_img = data_augment.augment(img_data, C, augment=False)
-
+                    
+                # check image size
                 (width, height) = (img_data_aug['width'], img_data_aug['height'])
                 (rows, cols, _) = x_img.shape
-
                 assert cols == width
                 assert rows == height
 
@@ -325,13 +355,13 @@ def get_anchor_gt(all_img_data, class_count, C, backend, mode='train'):
                 # resize the image so that smalles side is length = 600px
                 x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
 
+                # calculate rpn
                 try:
                     y_rpn_cls, y_rpn_regr = calc_rpn(C, img_data_aug, width, height, resized_width, resized_height)
                 except:
                     continue
 
                 # Zero-center by mean pixel, and preprocess image
-
                 x_img = x_img[:,:, (2, 1, 0)]  # BGR -> RGB
                 x_img = x_img.astype(np.float32)
                 x_img[:, :, 0] -= C.img_channel_mean[0]
@@ -339,11 +369,12 @@ def get_anchor_gt(all_img_data, class_count, C, backend, mode='train'):
                 x_img[:, :, 2] -= C.img_channel_mean[2]
                 x_img /= C.img_scaling_factor
 
-                x_img = np.transpose(x_img, (2, 0, 1))
+                x_img = np.transpose(x_img, (2, 0, 1)) # RGB -> BRG
                 x_img = np.expand_dims(x_img, axis=0)
 
                 y_rpn_regr[:, y_rpn_regr.shape[1]//2:, :, :] *= C.std_scaling
 
+                # change channel sequence for tensorflow backend
                 if backend == 'tf':
                     x_img = np.transpose(x_img, (0, 2, 3, 1))
                     y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1))
--- C5_FasterRCNN_KERAS/train_frcnn.py
+++ C5_FasterRCNN_KERAS/train_frcnn.py
@@ -15,14 +15,16 @@ from keras_frcnn import config, data_generators
 from keras_frcnn import losses as losses
 from keras_frcnn import resnet as nn
 import keras_frcnn.roi_helpers as roi_helpers
+from keras_frcnn.pascal_voc_parser import get_data
 from keras.utils import generic_utils
+
 
 sys.setrecursionlimit(40000)
 
-parser = OptionParser()
 
+# 训练参数设置及获取
+parser = OptionParser()
 parser.add_option("-p", "--path", dest="train_path", help="Path to training data.", default="data/")
-parser.add_option("-o", "--parser", dest="parser", help="Parser to use. One of simple or pascal_voc", default="pascal_voc"),
 parser.add_option("-n", "--num_rois", dest="num_rois", help="Number of ROIs per iteration. Higher means more memory use.", default=32)
 parser.add_option("--hf", dest="horizontal_flips", help="Augment with horizontal flips in training. (Default=true).", action="store_true", default=False)
 parser.add_option("--vf", dest="vertical_flips", help="Augment with vertical flips in training. (Default=false).", action="store_true", default=False)
@@ -31,63 +33,46 @@ parser.add_option("--num_epochs", dest="num_epochs", help="Number of epochs.", d
 parser.add_option("--config_filename", dest="config_filename", help="Location to store all the metadata related to the training (to be used when testing).", default="config/config.pickle")
 parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='model/model_frcnn.hdf5')
 parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights. If not specified, will try to load default weights provided by keras.")
-
 (options, args) = parser.parse_args()
 
-if not options.train_path:   # if filename is not given
-    parser.error('Error: path to training data must be specified. Pass --path to command line')
 
-if options.parser == 'pascal_voc':
-    from keras_frcnn.pascal_voc_parser import get_data
-elif options.parser == 'simple':
-    from keras_frcnn.simple_parser import get_data
-else:
-    raise ValueError("Command line option parser must be one of 'pascal_voc' or 'simple'")
-
-# pass the settings from the command line, and persist them in the config object
-C = config.Config()
-
-C.num_rois = int(options.num_rois)
-C.use_horizontal_flips = bool(options.horizontal_flips)
-C.use_vertical_flips = bool(options.vertical_flips)
-C.rot_90 = bool(options.rot_90)
-
-C.model_path = options.output_weight_path
-
-if options.input_weight_path:
-    C.base_net_weights = options.input_weight_path
-
+# read training data
 all_imgs, classes_count, class_mapping = get_data(options.train_path)
-
 if 'bg' not in classes_count:
     classes_count['bg'] = 0
     class_mapping['bg'] = len(class_mapping)
 
+# get settings from command line, and save them into Config
+C = config.Config()
+C.num_rois = int(options.num_rois)
+C.use_horizontal_flips = bool(options.horizontal_flips)
+C.use_vertical_flips = bool(options.vertical_flips)
+C.rot_90 = bool(options.rot_90)
+C.model_path = options.output_weight_path
+if options.input_weight_path:
+    C.base_net_weights = options.input_weight_path
 C.class_mapping = class_mapping
 
-inv_map = {v: k for k, v in class_mapping.items()}
-
+# print info for training data
 print('Training images per class:')
 pprint.pprint(classes_count)
 print('Num classes (including bg) = {}'.format(len(classes_count)))
 
+# generate Config file for test phase
 config_output_filename = options.config_filename
-
 with open(config_output_filename, 'wb') as config_f:
     pickle.dump(C,config_f)
     print('Config has been written to {}, and can be loaded when testing to ensure correct results'.format(config_output_filename))
 
+# shuffle data randomlyl, and split them into two parts: train, val
 random.shuffle(all_imgs)
-
-num_imgs = len(all_imgs)
-
 train_imgs = [s for s in all_imgs if s['imageset'] == 'trainval']
 val_imgs = [s for s in all_imgs if s['imageset'] == 'test']
-
 print('Num train samples {}'.format(len(train_imgs)))
 print('Num val samples {}'.format(len(val_imgs)))
 
-
+# generate positive and negative rpn anchors for groungtruth bbox
+# K.image_dim_ordering() is to get the backend name string ('tf' for tensorflow)
 data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, K.image_dim_ordering(), mode='train')
 #data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, C, K.image_dim_ordering(), mode='val')
 
@@ -141,7 +126,7 @@ start_time = time.time()
 
 best_loss = np.Inf
 
-class_mapping_inv = {v: k for k, v in class_mapping.items()}
+
 print('Starting training')
 
 
