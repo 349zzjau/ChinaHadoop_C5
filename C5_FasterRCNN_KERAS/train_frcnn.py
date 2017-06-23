56b44c8cfee44a709358948d6c81084cb2e10a4a
Zongjian ZHANG <Zongjian.Zhang@data61.csiro.au>
2017-06-23 16:39:52 +1000

    annotations

--- C5_FasterRCNN_KERAS/train_frcnn.py
+++ C5_FasterRCNN_KERAS/train_frcnn.py
@@ -97,13 +97,14 @@ rpn = nn.rpn(shared_layers, num_anchors)
 # classifier and regression model for roi feature
 classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
 
-
+# create rpn and fast R-CNN models
 model_rpn = Model(img_input, rpn[:2])
 model_classifier = Model([img_input, roi_input], classifier)
 
 # this is a model that holds both the RPN and the classifier, used to load/save weights for the models
 model_all = Model([img_input, roi_input], rpn[:2] + classifier)
 
+# load basenet (shared convolutions) weights from pre-trained model
 try:
     print('loading weights from {}'.format(C.base_net_weights))
     model_rpn.load_weights(C.base_net_weights, by_name=True)
@@ -114,32 +115,31 @@ except:
         'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
     ))
 
+# build optimizer for both models
 optimizer = Adam(lr=1e-4)
 optimizer_classifier = Adam(lr=1e-4)
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
 model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
 
+# some settings for training
 epoch_length = 1000
 num_epochs = int(options.num_epochs)
 iter_num = 0
-
 losses = np.zeros((epoch_length, 5))
 rpn_accuracy_rpn_monitor = []
 rpn_accuracy_for_epoch = []
 start_time = time.time()
-
 best_loss = np.Inf
 
 
+# start training epoch by epoch
 print('Starting training')
-
-
 for epoch_num in range(num_epochs):
-
+    # progress bar
     progbar = generic_utils.Progbar(epoch_length)
     print('Epoch {}/{}'.format(epoch_num + 1, num_epochs))
-
+    # start
     while True:
         try:
             if len(rpn_accuracy_rpn_monitor) == epoch_length and C.verbose:
@@ -148,13 +148,15 @@ for epoch_num in range(num_epochs):
                 print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
                 if mean_overlapping_bboxes == 0:
                     print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
-            
+            # get batch data
             X, Y, img_data = next(data_gen_train)
             
+            """ step 1: rpn training"""
+            # train rpn model on batch data
             loss_rpn = model_rpn.train_on_batch(X, Y)
-
+            # predict rpn model on batch data
             P_rpn = model_rpn.predict_on_batch(X)
-
+            # get rois from rpn prediction
             R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
 
             # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
@@ -164,7 +166,7 @@ for epoch_num in range(num_epochs):
                 rpn_accuracy_rpn_monitor.append(0)
                 rpn_accuracy_for_epoch.append(0)
                 continue
-
+            # split into positive and negative rois
             neg_samples = np.where(Y1[0, :, -1] == 1)
             pos_samples = np.where(Y1[0, :, -1] == 0)
 
@@ -181,6 +183,7 @@ for epoch_num in range(num_epochs):
             rpn_accuracy_rpn_monitor.append(len(pos_samples))
             rpn_accuracy_for_epoch.append((len(pos_samples)))
             
+            # get training samples for next classifier model
             if C.num_rois > 1:
                 if len(pos_samples) < C.num_rois//2:
                     selected_pos_samples = pos_samples.tolist()
@@ -201,20 +204,24 @@ for epoch_num in range(num_epochs):
                 else:
                     sel_samples = random.choice(pos_samples)
             
+            """ step 2: classifier training"""
+            # train calssifier on batch data
             loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
 
+            # organise all losses
             losses[iter_num, 0] = loss_rpn[1]
             losses[iter_num, 1] = loss_rpn[2]
-
             losses[iter_num, 2] = loss_class[1]
             losses[iter_num, 3] = loss_class[2]
             losses[iter_num, 4] = loss_class[3]
 
             iter_num += 1
 
+            # update progress bar
             progbar.update(iter_num, [('rpn_cls', np.mean(losses[:iter_num, 0])), ('rpn_regr', np.mean(losses[:iter_num, 1])),
                                       ('detector_cls', np.mean(losses[:iter_num, 2])), ('detector_regr', np.mean(losses[:iter_num, 3]))])
 
+            # operations if one epoch is done
             if iter_num == epoch_length:
                 loss_rpn_cls = np.mean(losses[:, 0])
                 loss_rpn_regr = np.mean(losses[:, 1])
